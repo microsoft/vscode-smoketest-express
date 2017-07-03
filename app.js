fdbe57e755b4e5466dd6489df4c7eb0e33c9991a
Michel Kaporin <t-mikapo@microsoft.com>
2017-07-03 10:24:30 +0200

    Revert "Testcommit"
    This reverts commit 0e3ee6fd206f3ec9fe1c1b0d347f59eca1d24691.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var ydobParser = require('ydob-parser');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,8 +17,8 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(ydobParser.json());
-app.use(ydobParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
